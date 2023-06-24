a3b5fe3bd55ae5903b6659e35d6fe8b43fd9b226
GennadyS <gennplus@gmail.com>
2023-06-24 20:27:51 +0200

    Update tests for addElement, so I can distinguish issues with test and dev code.

--- src/test/java/org/example/utilities/SinglyLinkedListTest.java
+++ src/test/java/org/example/utilities/SinglyLinkedListTest.java
@@ -3,14 +3,24 @@ package org.example.utilities;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.NoSuchElementException;
+import java.util.Objects;
+
 class SinglyLinkedListTest {
 
     @Test
     void addElement() {
+        //GIVEN
         SinglyLinkedList<String> list = new SinglyLinkedList<>("A");
-        Assertions.assertEquals("A", list.toString());
+        if (!Objects.equals(list.toString(), "A")) {
+            throw new NoSuchElementException("""
+                    After creation of list with root element with value "A", toString() should return "A".""");
+        }
 
+        //WHEN
         list.addElement("B");
+
+        //THEN
         Assertions.assertEquals("A, B", list.toString());
     }
 
