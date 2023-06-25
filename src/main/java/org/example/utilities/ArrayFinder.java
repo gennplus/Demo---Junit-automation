ce7046ad8c5a5fceb2e4e0fce0ea446cd2269c13
GennadyS <gennplus@gmail.com>
2023-06-25 16:39:52 +0200

    Implement findArray() method.

--- src/main/java/org/example/utilities/ArrayFinder.java
+++ src/main/java/org/example/utilities/ArrayFinder.java
@@ -1,10 +1,15 @@
 package org.example.utilities;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ArrayFinder {
 
     public static List<Integer[]> findArray(List<Integer[]> arrays, Integer numberToFind) {
-        return null;
+        return arrays.stream()
+                .filter(array -> Arrays.stream(array)
+                        .anyMatch(integer -> integer.equals(numberToFind)))
+                .collect(Collectors.toList());
     }
 }
